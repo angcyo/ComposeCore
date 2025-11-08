63711d6da5cd2b228b172a7cd7ec618581681c3c
angcyo <angcyo@126.com>
2025-11-08 18:43:54 +0800

    init

--- basics/src/main/java/com/angcyo/compose/basics/ContextEx.kt
+++ basics/src/main/java/com/angcyo/compose/basics/ContextEx.kt
@@ -1,7 +1,10 @@
 package com.angcyo.compose.basics
 
 import android.content.Context
+import android.content.Intent
+import android.provider.Settings
 import android.widget.Toast
+import androidx.core.net.toUri
 
 /**
  * @author <a href="mailto:angcyo@126.com">angcyo</a>
@@ -11,4 +14,20 @@ import android.widget.Toast
 /**使用Android原生, 弹出[toast]提示*/
 fun Context.toast(message: CharSequence, duration: Int = Toast.LENGTH_SHORT) {
     Toast.makeText(this, message, duration).show()
+}
+
+/**请求忽略电池优化
+ * ```
+ * <uses-permission android:name="android.permission.REQUEST_IGNORE_BATTERY_OPTIMIZATIONS" />
+ * ```
+ * 谷歌市场提示:
+ * ```
+ * Use of `REQUEST_IGNORE_BATTERY_OPTIMIZATIONS` violates the Play Store Content Policy regarding acceptable use cases,
+ * as described in https://developer.android.com/training/monitoring-device-state/doze-standby.html
+ * ```
+ * */
+fun Context.requestIgnoreBatteryOptimizations() {
+    val intent = Intent(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS)
+    intent.data = "package:${packageName}".toUri()
+    startActivity(intent)
 }
