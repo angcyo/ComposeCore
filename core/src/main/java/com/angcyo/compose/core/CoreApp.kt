f952414c694a9d4286ba15d279c614475f67c750
angcyo <angcyo@126.com>
2025-11-09 10:50:35 +0800

    init

--- core/src/main/java/com/angcyo/compose/core/CoreApp.kt
+++ core/src/main/java/com/angcyo/compose/core/CoreApp.kt
@@ -3,16 +3,19 @@ package com.angcyo.compose.core
 import androidx.annotation.Keep
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.PreviewScreenSizes
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.navigation3.rememberViewModelStoreNavEntryDecorator
 import androidx.navigation3.runtime.NavBackStack
 import androidx.navigation3.runtime.NavEntry
 import androidx.navigation3.runtime.NavKey
@@ -69,14 +72,6 @@ var unknownScene = SceneRoute(
  * */
 val LocalNavBackStack = compositionLocalOf<NavBackStack<NavKey>?> { null }
 
-// 定义 CompositionLocal（可为 null 或有默认）
-data class User(val id: Int, val name: String)
-
-val LocalUser = compositionLocalOf<User?> { null } // 默认 null
-
-val LocalValue = compositionLocalOf { 10 }
-val LocalLargerValue = compositionLocalOf { 12 }
-
 /**启动一个带有导航的应用
  * - [Dialog]
  * - [Popup]
@@ -93,52 +88,41 @@ fun RunNavApp(
                     modifier = Modifier.fillMaxSize(), // 背景颜色
                     contentAlignment = Alignment.Center
                 ) {
-                    Text("Hello Jetpack Compose")
+                    val backStack = LocalNavBackStack.current;
+                    Text(
+                        text = "Hello Jetpack Compose",
+                        modifier = Modifier.clickable(onClick = { backStack?.removeLastOrNull() })
+                    )
                 }
             },
         )
     ),
 ) {
     val backStack = rememberNavBackStack(*routers.toTypedArray())
-    val local = LocalNavBackStack provides backStack
-
-    /*CompositionLocalProvider(local,local) {
-
-    }*/
-
-    // 在根处提供一个 user
-    /*CompositionLocalProvider(ProvidedValue()) {
-
+    CompositionLocalProvider(LocalNavBackStack provides backStack) {
+        NavDisplay(
+            backStack,
+            entryDecorators = listOf(
+                // Add the default decorators for managing scenes and saving state
+                rememberSaveableStateHolderNavEntryDecorator(),
+                // Then add the view model store decorator
+                rememberViewModelStoreNavEntryDecorator()
+            ),
+            onBack = {
+                if (backStack.isNotEmpty()) {
+                    backStack.removeLastOrNull()
+                }
+            },
+            entryProvider = entryProvider({
+                val scene = unknownScene
+                NavEntry(scene) {
+                    scene.content()
+                }
+            }) {
+                entry<SceneRoute> {
+                    it.content()
+                }
+            },
+        )
     }
-
-    CompositionLocalProvider(
-        LocalLargerValue providesComputed { LocalValue.currentValue + 10 }
-    ) {
-
-    }*/
-
-    NavDisplay(
-        backStack,
-        entryDecorators = listOf(
-            // Add the default decorators for managing scenes and saving state
-            rememberSaveableStateHolderNavEntryDecorator(),
-            // Then add the view model store decorator
-            /*rememberViewModelStoreNavEntryDecorator()*/
-        ),
-        onBack = {
-            if (backStack.isNotEmpty()) {
-                backStack.removeLastOrNull()
-            }
-        },
-        entryProvider = entryProvider({
-            val scene = unknownScene
-            NavEntry(scene) {
-                scene.content()
-            }
-        }) {
-            entry<SceneRoute> {
-                it.content()
-            }
-        },
-    )
 }
